867568a00a668b77c9947a90f29009e984961822
nathan <grimaudathan@gmail.com>
2019-02-08 11:33:27 +0100

    fix: port

--- src/server.js
+++ src/server.js
@@ -3,27 +3,27 @@ import HapiSwagger from 'hapi-swagger'
 import Package from '../package.json'
 
 const config = {
-  api: {
-    host: '0.0.0.0',
-    port: 3000
-  }
+    api: {
+        host: '0.0.0.0',
+        port: 8080
+    }
 }
 
 const server = new Hapi.Server({
-  host: config.api.host,
-  port: config.api.port,
-  routes: { cors: { origin: ['*'] } }
+    host: config.api.host,
+    port: config.api.port,
+    routes: { cors: { origin: ['*'] } }
 })
 
 const swaggerOptions = {
-  title: 'Test API Documentation',
-  info: {
-    version: Package.version
-  }
+    title: 'Test API Documentation',
+    info: {
+        version: Package.version
+    }
 }
 export const Swagger = {
-  plugin: HapiSwagger,
-  options: swaggerOptions
+    plugin: HapiSwagger,
+    options: swaggerOptions
 }
 
 export default server
